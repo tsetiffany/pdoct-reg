70e087c750652b83b7c31848be34f5cd7bfe9dd2
tsetiffany <tiffanytsepiano@gmail.com>
2024-12-04 16:54:51 -0800

    modified to do axial matching by batches

--- MATLAB scripts/mcorrLocal_axial.m
+++ MATLAB scripts/mcorrLocal_axial.m
@@ -1,26 +1,25 @@
-function [vol_mcorr, yshift, xshift] = mcorrLocal_axial(fixed, reg)
+function [vol_mcorr, yshift, xshift] = mcorrLocal_axial(fixed, reg, numBatch)
 
     numPoints   = size(fixed, 1);
     numLines    = size(fixed, 2);
     numFrames   = size(fixed, 3);   % For global motion correction
-    
-    kLinear         = linspace(-1,1,numPoints);
-    kaxis           = repmat(kLinear',1,numLines);
-    
+     
+    sizeBatch = ceil(numLines/numBatch); 
     vol_mcorr = reg;
     
     for I = 1:numFrames
-        % compare fixed and moving frame by frame
-        [output] = dftregistration(fft2(abs(fixed(:,:,I))),...
-            fft2(abs(reg(:,:,I))));
-        yshift(I) = output(3);
-        xshift(I) = output(4);
+        for j=1:numBatch
+            
+           % Determine start and end index of each batch
+            startIdx = (j-1)*sizeBatch + 1;
+            endIdx = startIdx + sizeBatch - 1;
 
-        % This line does the lateral shift
-        % temp = ifft2(fft2(vol(:,:,I)).*cplx_shift);
-
-        % Only do axial correction
-%         vol_mcorr(:,:,I) = fft(ifft(reg(:,:,I)).*exp(2j.*(output(3)*(kaxis))));
-        vol_mcorr(:,:,I) = circshift(fft(ifft(reg(:,:,I))), [output(3), 0]); 
+            [output] = dftregistration(fft2(abs(fixed(:, startIdx:endIdx, I))),...
+                fft2(abs(reg(:, startIdx:endIdx, I))));
+            yshift(I) = output(3);
+            xshift(I) = output(4);
+            
+            vol_mcorr(:,startIdx:endIdx,I) = circshift(fft(ifft(reg(:, startIdx:endIdx, I))), [output(3), 0]); 
+        end
     end
 end

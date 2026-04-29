37be666860f7f197edfe76e33fb0ad347d771a98
tsetiffany <73195357+tsetiffany@users.noreply.github.com>
2026-04-29 14:57:34 -0700

    minor changes

--- MATLAB scripts/mainPostProc.m
+++ MATLAB scripts/mainPostProc.m
@@ -8,7 +8,7 @@
 
 %% COMPLETE REGISTRATION
 
-filepath = 'I:\1. Reg_Manuscript\MJ_20250618_FOV_reg\UWFOV\Reg\outputs'; % Update this to Python output directory
+filepath = 'H:\PS391\OD\Reg\outputs'; % Update this to Python output directory
 numBatch = 60;
 
 % Axial matching
@@ -18,14 +18,15 @@ output_filepath = axialMatching(filepath,numBatch);
 [averaged_oct, averaged_dopu] = averageVolumes(output_filepath);
 [depth, numAscans, numBscans] = size(averaged_dopu); 
 
-%% GENERATE DOPU COMPOSITE
+% GENERATE DOPU COMPOSITE
 
 cmap_dopu_r = load('cmap_dopu_r.mat').cmap_dopu_r;    %load the DOPU colormap
-DOPU_Bscans = genDOPUComposite(averaged_dopu(:,26:end-25,:),averaged_oct(:,26:end-25,:),cmap_dopu_r);
+% DOPU_Bscans = genDOPUComposite(averaged_dopu(:,26:end-25,:),averaged_oct(:,26:end-25,:),cmap_dopu_r);
+DOPU_Bscans = genDOPUComposite(averaged_dopu(:,:,:),averaged_oct(:,:,:),cmap_dopu_r);
 
 disp('Saving DOPU composite...')
 for i=1:size(averaged_dopu,3)
-    imshow(flipud(DOPU_Bscans(:,:,:,i))); colormap(cmap_dopu_r)
+%     imshow(flipud(DOPU_Bscans(:,:,:,i))); colormap(cmap_dopu_r)
     imwrite(uint8(255*flipud(DOPU_Bscans(:,:,:,i))),cmap_dopu_r,fullfile(output_filepath,'FINAL_DOPU_COMP_reg_avg_volume.tif'),'WriteMode','append');
 end
 
@@ -42,8 +43,13 @@ imgF=mat2gray(imgF);
 imInd = gray2ind(imgF,256);
 img_RGB=ind2rgb(imInd,hot(256));
 
-figure;imshow(img_RGB(21:580,21:580));colormap(hot); 
-exportgraphics(gcf,fullfile(output_filepath,'\registered_dopu.tif'));
+figure;imshow(img_RGB(:,:,:));colormap(hot); 
+exportgraphics(gcf,fullfile(output_filepath,'\fixed_registered_dopu.tif'));
+
+%% Save registered OCT en-face
+
+figure;imshow(imadjust(mat2gray(imrotate(fliplr(squeeze(mean(averaged_oct(:,:,:)))),90))));
+exportgraphics(gcf,fullfile(output_filepath,'\fixed_registered_oct.tif'));
 
 %% OPTIONAL: RPE SEGMENTATION
 % the image looks the same with or without segmentation line
@@ -98,14 +104,19 @@ for i=1:600
 end          
 
 %% SAVING FOR VISUALIZATION (PPT VIDEO ONLY)
+cmap_dopu_r = load('cmap_dopu_r.mat').cmap_dopu_r;    %load the DOPU colormap
 
-fixed_DOPU_Bscans = genDOPUComposite(DOPU_mcorr(:,26:end-25,:),OCT_mcorr(:,26:end-25,:),cmap_dopu_r);
-for i=21:2:size(averaged_dopu,3)-20 % use every other frame to make the size consistent with 2FA processed data / shorter video. crop out noise slices, typically 20 frames
-%     imshow(flipud(DOPU_Bscans(:,:,:,i))); colormap(cmap_dopu_r)
-%     imwrite(uint8(255*fixed_DOPU_Bscans(:,:,:,i)),cmap_dopu_r,fullfile(output_filepath,'FINAL_fixed_CROPPED_DOPU_COMP_reg_avg_volume.tif'),'WriteMode','append');
-%     imwrite(uint8(255*flipud(DOPU_Bscans(:,:,:,i))),cmap_dopu_r,fullfile(output_filepath,'FINAL_CROPPED_DOPU_COMP_reg_avg_volume.tif'),'WriteMode','append');
-    imwrite(uint8(255* imadjust(mat2gray(averaged_oct(:,:,i)))),fullfile(output_filepath,'FINAL_CROPPED_OCT_reg_avg_volume.tif'),'WriteMode','append');
-    imwrite(uint8(255* imadjust(mat2gray(flipud(OCT_mcorr(:,:,i))))),fullfile(output_filepath,'FINAL_fixed_CROPPED_OCT_reg_avg_volume.tif'),'WriteMode','append');
+fixed_DOPU_Bscans = genDOPUComposite(fixed_dopu(:,36:end-35,:),fixed(:,36:end-35,:),cmap_dopu_r);
+reg_DOPU_Bscans = genDOPUComposite(axmat_dopu(:,14:520,:),axmat_oct(:,14:520,:),cmap_dopu_r);
+output_filepath = 'H:\For Jiwon\OPTH_RD_2026\melanoma1\outputs\registered_mat_files';
+for i=1:size(axmat_dopu,3) % use every other frame to make the size consistent with 2FA processed data / shorter video. crop out noise slices, typically 20 frames
+% %  imshow(flipud(reg_DOPU_Bscans(:,:,:,399))); colormap(cmap_dopu_r)
+%     imwrite(uint8(255*reg_DOPU_Bscans(:,:,:,i)),cmap_dopu_r,fullfile(output_filepath,'t2_FINAL_DOPU_COMP_reg_volume.tif'),'WriteMode','append');
+    imwrite(uint8(255*fixed_DOPU_Bscans(:,:,:,i)),cmap_dopu_r,fullfile(output_filepath,'t3_DOPU_COMP_reg_volume.tif'),'WriteMode','append');
+% %     imwrite(uint8(255*flipud(DOPU_Bscans(:,:,:,i))),cmap_dopu_r,fullfile(output_filepath,'FINAL_CROPPED_DOPU_COMP_reg_avg_volume.tif'),'WriteMode','append');
+% %     imwrite(uint8(255* imadjust(mat2gray(averaged_oct(:,:,i)))),fullfile(output_filepath,'FINAL_CROPPED_OCT_reg_avg_volume.tif'),'WriteMode','append');
+%     imwrite(uint8(255* imadjust(mat2gray(flipud(axmat_oct(:,14:520,i))))),fullfile(output_filepath,'t2_FINAL_OCT_reg_volume.tif'),'WriteMode','append');
+    imwrite(uint8(255* imadjust(mat2gray(flipud(fixed(:,14:520,i))))),fullfile(output_filepath,'t3_FINAL_OCT_reg_volume.tif'),'WriteMode','append');
 end
 
 
